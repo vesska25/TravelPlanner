7fa8a05c10c2504a36974b41f7016373316d65bc
Sergei Savich <sergesvc@gmail.com>
2026-06-15 12:33:55 +0200

    Initial project setup: Spring Boot 4.1, Postgres via Docker

---
+++ src/test/java/de/sergio/TravelPlanner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sergio.TravelPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
